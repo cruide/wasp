0231242cc2992f5eb61b745626824b89ebea3470
Alex Tisch <info@alex-tisch.ru>
2016-02-01 09:19:52 +0300

    Update framework

--- app/i18n/auth/en.php
+++
@@ -1,4 +0,0 @@
-<?php return [
-    'authError'     => 'Authorization error',
-    'authorization' => 'Authorization',
-];
--- app/i18n/profile/ru.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    'user_profile' => 'Профиль пользователя',
-    'first_name'   => 'Имя',
-    'middle_name'  => 'Отчество',
-    'last_name'    => 'Фамилия',
-];

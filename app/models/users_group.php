6c32c671e327e0a530e50f3b9e4968ec202a906e
Alex Tisch <info@alex-tisch.ru>
2016-02-11 09:41:18 +0300

    Restruct

--- app/models/users_group.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Models;
-
-class UsersGroup extends \Wasp\Model
-{
-    public function users()
-    {
-        return $this->hasMany('\\App\\Models\\User');
-    }
-}
--- app/models/users_session.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Models;
-
-class UsersSession extends \Wasp\Model
-{
-    public function user()
-    {
-        return $this->belongsTo('\\App\\Models\\User');
-    }
-}
